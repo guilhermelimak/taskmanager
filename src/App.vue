81927f7d8a2d75d12f84f9c4ab495b5713ff59e7
Guilherme Lima <guilhermelimak@gmail.com>
2016-08-31 21:50:54 -0300

    Add bulma

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,22 @@
 </template>
 
 <script>
+import Dashboard from 'components/Dashboard.vue'
 import store from './store'
 
 export default {
   el: 'body',
   store,
   replace: false,
+  components: {
+    Dashboard,
+  },
 }
 </script>
 
 <style>
+@import '../node_modules/bulma/css/bulma.css';
+
 html,
 body {
   height:  100%;
--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -20,7 +20,7 @@ export default {
     display: flex;
     flex-direction: row;
     height: 100%;
-    padding: 20px
+    padding: 20px;
     width: 100%;
   }
 }
