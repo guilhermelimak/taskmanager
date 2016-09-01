71ba7d0f09bd485db80c0c85eb97dfbdfa06e280
Guilherme Lima <guilhermelimak@gmail.com>
2016-08-31 21:58:14 -0300

    Add firebase

--- src/store.js
+++ src/store.js
@@ -2,9 +2,28 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import mutations from 'mutations'
 
+import config from '../config'
+
 Vue.use(Vuex)
 
-const state = {}
+const state = {
+  loading: true,
+  lists: [],
+}
+
+const firebase = require('firebase/app')
+require('firebase/database')
+
+firebase.initializeApp(config)
+export const database = firebase.database()
+
+database
+.ref()
+.once('value')
+.then(snapshot => {
+  state.lists = snapshot.val().lists
+  state.loading = false
+})
 
 export default new Vuex.Store({
   state,
