00835777205f07f2c8f06c858785cea572203c74
Guilherme Lima <guilhermelimak@gmail.com>
2016-10-01 04:03:05 -0300

    Add config file
    Remove useles npm install.

---
+++ config.js
@@ -0,0 +1,13 @@
+export const prod = {
+  apiKey: process.env.APIKEY,
+  authDomain: 'taskmanager-ad463.firebaseapp.com',
+  databaseURL: 'http://taskmanager-ad463.firebaseio.com',
+  storageBucket: 'taskmanager-ad463.appspot.com',
+}
+
+export const dev = {
+  apiKey: 'AIzaSyAofKnunFDTV4Bwfb2Tjx1GGFUz-0Ujo-g',
+  authDomain: 'task-manager-develop.firebaseapp.com',
+  databaseURL: 'https://task-manager-develop.firebaseio.com',
+  storageBucket: 'task-manager-develop.appspot.com',
+}
